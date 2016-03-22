db396d6f32f65e2900b11befe52b91f9967f3642
Alexander Agnarson <alexander.agnarson@gmail.com>
2016-03-22 13:00:33 +0100

    escape

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form method="get" class="searchform themeform" action="<?php echo home_url('/'); ?>">
+<form method="get" class="searchform themeform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div>
 		<input type="text" class="search" name="s" onblur="if(this.value=='')this.value='<?php _e('To search type and hit enter','kontrast'); ?>';" onfocus="if(this.value=='<?php _e('To search type and hit enter','kontrast'); ?>')this.value='';" value="<?php _e('To search type and hit enter','kontrast'); ?>" />
 	</div>
