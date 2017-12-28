440276d61d4e147c647ed5ebc53333ba29cb0ce9
Alexander Agnarson <alexander.agnarson@gmail.com>
2017-12-28 12:23:59 +0100

    Fix featured posts

--- inc/featured.php
+++ inc/featured.php
@@ -6,13 +6,13 @@ $featured = new WP_Query(
 		'update_post_meta_cache'	=> false,
 		'update_post_term_cache'	=> false,
 		'ignore_sticky_posts'		=> 1,
-		'posts_per_page'			=> get_theme_mod('featured-posts-count','1'),
+		'posts_per_page'			=> get_theme_mod('featured-posts-count','3'),
 		'cat'						=> get_theme_mod('featured-category','')
 	)
 );
 ?>
 
-<?php if ( is_home() && !is_paged() && ( get_theme_mod('featured-posts-count','1') =='1') ): // No slider if 1 post is featured ?>
+<?php if ( is_home() && !is_paged() && ( get_theme_mod('featured-posts-count','3') =='1') ): // No slider if 1 post is featured ?>
 	
 	<div class="featured">
 		<?php while ( $featured->have_posts() ): $featured->the_post(); ?>
@@ -20,7 +20,7 @@ $featured = new WP_Query(
 		<?php endwhile; ?>	
 	</div><!--/.featured-->
 	
-<?php elseif ( is_home() && !is_paged() && ( get_theme_mod('featured-posts-count','1') !='0') ): // Show slider if posts are not 1 or 0 ?>
+<?php elseif ( is_home() && !is_paged() && ( get_theme_mod('featured-posts-count','3') !='0') ): // Show slider if posts are not 1 or 0 ?>
 	
 	<script type="text/javascript">
 		// Check if first slider image is loaded, and load flexslider on document ready
