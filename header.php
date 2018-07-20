9506c17895c265b557fe87c26578e7b7b883d29c
Alexander Agnarson <alexander.agnarson@gmail.com>
2018-07-20 13:53:02 +0200

    Remove unused shortcode css, fix header tab spacing

--- header.php
+++ header.php
@@ -23,16 +23,16 @@
 				<div class="nav-wrap container"><?php wp_nav_menu(array('theme_location'=>'mobile','menu_class'=>'nav container-inner group','container'=>'','menu_id' => '','fallback_cb'=> false)); ?></div>
 				
 				<?php if ( get_theme_mod( 'header-search', 'on' ) == 'on' ): ?>
-				<div class="container">
-					<div class="container-inner">		
-						<div class="toggle-search"><i class="fa fa-search"></i></div>
-						<div class="search-expand">
-							<div class="search-expand-inner">
-								<?php get_search_form(); ?>
+					<div class="container">
+						<div class="container-inner">		
+							<div class="toggle-search"><i class="fa fa-search"></i></div>
+							<div class="search-expand">
+								<div class="search-expand-inner">
+									<?php get_search_form(); ?>
+								</div>
 							</div>
-						</div>
-					</div><!--/.container-inner-->
-				</div><!--/.container-->
+						</div><!--/.container-inner-->
+					</div><!--/.container-->
 				<?php endif; ?>
 				
 			</nav><!--/#nav-mobile-->
@@ -45,41 +45,41 @@
 				<div class="nav-wrap container"><?php wp_nav_menu(array('theme_location'=>'topbar','menu_class'=>'nav container-inner group','container'=>'','menu_id' => '','fallback_cb'=> false)); ?></div>
 				
 				<?php if ( get_theme_mod( 'header-search', 'on' ) == 'on' ): ?>
-				<div class="container">
-					<div class="container-inner">		
-						<div class="toggle-search"><i class="fa fa-search"></i></div>
-						<div class="search-expand">
-							<div class="search-expand-inner">
-								<?php get_search_form(); ?>
+					<div class="container">
+						<div class="container-inner">		
+							<div class="toggle-search"><i class="fa fa-search"></i></div>
+							<div class="search-expand">
+								<div class="search-expand-inner">
+									<?php get_search_form(); ?>
+								</div>
 							</div>
-						</div>
-					</div><!--/.container-inner-->
-				</div><!--/.container-->
+						</div><!--/.container-inner-->
+					</div><!--/.container-->
 				<?php endif; ?>
 				
 			</nav><!--/#nav-topbar-->
 		<?php endif; ?>
 		
 		<?php if ( get_theme_mod('header-ads','off') == 'on' ): ?>
-		<div class="container" id="header-ads">
-			<div class="container-inner">
-				<?php dynamic_sidebar( 'header-ads' ); ?>
-			</div><!--/.container-inner-->
-		</div><!--/.container-->
+			<div class="container" id="header-ads">
+				<div class="container-inner">
+					<?php dynamic_sidebar( 'header-ads' ); ?>
+				</div><!--/.container-inner-->
+			</div><!--/.container-->
 		<?php endif; ?>
 		
 		<div class="container-inner group">
 			
 			<?php if ( get_header_image() == '' ) : ?>
-			<div class="group pad">
-				<?php echo kontrast_site_title(); ?>
-				 <?php if ( display_header_text() == true ): ?>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-				<?php endif; ?>
-				<?php if ( get_theme_mod( 'header-social', 'on' ) == 'on' ): ?>
-					<?php kontrast_social_links() ; ?>
-				<?php endif; ?>
-			</div>
+				<div class="group pad">
+					<?php echo kontrast_site_title(); ?>
+					<?php if ( display_header_text() == true ): ?>
+						<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+					<?php endif; ?>
+					<?php if ( get_theme_mod( 'header-social', 'on' ) == 'on' ): ?>
+						<?php kontrast_social_links() ; ?>
+					<?php endif; ?>
+				</div>
 			<?php endif; ?>
 			
 			<?php if ( get_header_image() ) : ?>
