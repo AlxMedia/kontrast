ee23487d31cae7d666042b03e90e0a2a5a57f773
Nilambar Sharma <nilambar@outlook.com>
2018-09-04 12:39:29 +0545

    Fix missing escaping

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 				</div><!--/.main-inner-->
-			</div><!--/.main-->			
+			</div><!--/.main-->
 		</div><!--/.container-inner-->
 	</div><!--/.container-->
 
 	<footer id="footer">
-			
+
 		<?php if ( get_theme_mod('footer-logo') ): ?>
-			<img id="footer-logo" src="<?php echo esc_url( get_theme_mod('footer-logo') ); ?>" alt="<?php get_bloginfo('name'); ?>">
+			<img id="footer-logo" src="<?php echo esc_url( get_theme_mod('footer-logo') ); ?>" alt="<?php echo esc_attr( get_bloginfo('name')); ?>">
 		<?php endif; ?>
-		
+
 		<?php if ( get_theme_mod('footer-ads','off') == 'on' ): ?>
 		<div class="container" id="footer-ads">
 			<div class="container-inner">
@@ -20,7 +20,7 @@
 		<?php // footer widgets
 			$total = 4;
 			if ( get_theme_mod( 'footer-widgets','0' ) != '' ) {
-				
+
 				$total = get_theme_mod( 'footer-widgets' );
 				if( $total == 1) $class = 'one-full';
 				if( $total == 2) $class = 'one-half';
@@ -31,27 +31,27 @@
 				if ( ( is_active_sidebar( 'footer-1' ) ||
 					   is_active_sidebar( 'footer-2' ) ||
 					   is_active_sidebar( 'footer-3' ) ||
-					   is_active_sidebar( 'footer-4' ) ) && $total > 0 ) 
-		{ ?>		
+					   is_active_sidebar( 'footer-4' ) ) && $total > 0 )
+		{ ?>
 		<div class="container dark" id="footer-widgets">
 			<div class="container-inner">
-				
+
 				<div class="pad group">
 					<?php $i = 0; while ( $i < $total ) { $i++; ?>
 						<?php if ( is_active_sidebar( 'footer-' . $i ) ) { ?>
-					
+
 					<div class="footer-widget-<?php echo esc_attr( $i ); ?> grid <?php echo esc_attr( $class ); ?> <?php if ( $i == $total ) { echo 'last'; } ?>">
 						<?php dynamic_sidebar( 'footer-' . $i ); ?>
 					</div>
-					
+
 						<?php } ?>
 					<?php } ?>
 				</div><!--/.pad-->
-				
+
 			</div><!--/.container-inner-->
-		</div><!--/.container-->	
+		</div><!--/.container-->
 		<?php } ?>
-		
+
 		<?php if ( has_nav_menu( 'footer' ) ): ?>
 			<nav class="nav-container group" id="nav-footer">
 				<div class="nav-toggle"><i class="fa fa-bars"></i></div>
@@ -59,43 +59,43 @@
 				<div class="nav-wrap"><?php wp_nav_menu( array('theme_location'=>'footer','menu_class'=>'nav container group','container'=>'','menu_id'=>'','fallback_cb'=>false) ); ?></div>
 			</nav><!--/#nav-footer-->
 		<?php endif; ?>
-		
+
 		<div class="container" id="footer-bottom">
 			<div class="container-inner">
-				
+
 				<a id="back-to-top" href="#"><i class="fa fa-angle-up"></i></a>
-				
+
 				<div class="pad group">
-					
+
 					<div class="grid one-half">
-						
+
 						<div id="copyright">
 							<?php if ( get_theme_mod( 'copyright' ) ): ?>
 								<p><?php echo esc_html( get_theme_mod( 'copyright' ) ); ?></p>
 							<?php else: ?>
-								<p><?php bloginfo(); ?> &copy; <?php echo date( 'Y' ); ?>. <?php esc_html_e( 'All Rights Reserved.', 'kontrast' ); ?></p>
+								<p><?php bloginfo(); ?> &copy; <?php echo esc_html( date_i18n( esc_html__( 'Y', 'kontrast' ) ) ); ?>. <?php esc_html_e( 'All Rights Reserved.', 'kontrast' ); ?></p>
 							<?php endif; ?>
 						</div><!--/#copyright-->
-						
+
 						<?php if ( get_theme_mod( 'credit', 'on' ) == 'on' ): ?>
 						<div id="credit">
 							<p><?php esc_html_e('Powered by','kontrast'); ?> <a href="http://wordpress.org" rel="nofollow">WordPress</a>. <?php esc_html_e('Theme by','kontrast'); ?> <a href="http://alxmedia.se" rel="nofollow">Alx</a>.</p>
 						</div><!--/#credit-->
 						<?php endif; ?>
-						
+
 					</div>
-					
-					<div class="grid one-half last">	
+
+					<div class="grid one-half last">
 						<?php if ( get_theme_mod( 'footer-social', 'on' ) == 'on' ): ?>
 							<?php kontrast_social_links() ; ?>
 						<?php endif; ?>
 					</div>
-				
+
 				</div><!--/.pad-->
-				
+
 			</div><!--/.container-inner-->
 		</div><!--/.container-->
-		
+
 	</footer><!--/#footer-->
 
 </div><!--/#wrapper-->
--- inc/featured.php
+++ inc/featured.php
@@ -6,31 +6,31 @@ $featured = new WP_Query(
 		'update_post_meta_cache'	=> false,
 		'update_post_term_cache'	=> false,
 		'ignore_sticky_posts'		=> 1,
-		'posts_per_page'			=> get_theme_mod('featured-posts-count','3'),
-		'cat'						=> get_theme_mod('featured-category','')
+		'posts_per_page'			=> absint( get_theme_mod('featured-posts-count','3') ),
+		'cat'						=> absint( get_theme_mod('featured-category','') )
 	)
 );
 ?>
 
 <?php if ( is_home() && !is_paged() && ( get_theme_mod('featured-posts-count','3') =='1') ): // No slider if 1 post is featured ?>
-	
+
 	<div class="featured">
 		<?php while ( $featured->have_posts() ): $featured->the_post(); ?>
 			<?php get_template_part('content-featured'); ?>
-		<?php endwhile; ?>	
+		<?php endwhile; ?>
 	</div><!--/.featured-->
-	
+
 <?php elseif ( is_home() && !is_paged() && ( get_theme_mod('featured-posts-count','3') !='0') ): // Show slider if posts are not 1 or 0 ?>
-		
+
 	<div class="featured flexslider" id="flexslider-featured">
-		<ul class="slides">				
+		<ul class="slides">
 			<?php while ( $featured->have_posts() ): $featured->the_post(); ?>
-			<li>	
+			<li>
 				<?php get_template_part('content-featured'); ?>
 			</li>
-			<?php endwhile; ?>			
+			<?php endwhile; ?>
 		</ul>
 	</div><!--/.featured-->
-	
+
 <?php endif; ?>
 <?php wp_reset_postdata(); ?>
