c73924ff24be03ea2b57d3de11e71f20163203b9
AlxMedia <contact@alx.media>
2022-02-23 12:09:28 +0100

    Update FontAwesome customizer link to v5 brands

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -631,7 +631,7 @@ Kirki::add_field( 'kontrast_theme', array(
 	'label'			=> esc_html__( 'Create Social Links', 'kontrast' ),
 	'description'	=> esc_html__( 'Create and organize your social links', 'kontrast' ),
 	'section'		=> 'social',
-	'tooltip'		=> esc_html__( 'Font Awesome names:', 'kontrast' ) . ' <a href="http://fortawesome.github.io/Font-Awesome/icons/" target="_blank"><strong>' . esc_html__( 'View All', 'kontrast' ) . ' </strong></a>',
+	'tooltip'		=> esc_html__( 'Font Awesome names:', 'kontrast' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'kontrast' ) . ' </strong></a>',
 	'row_label'		=> array(
 		'type'	=> 'text',
 		'value'	=> esc_html__('social link', 'kontrast' ),
@@ -648,7 +648,7 @@ Kirki::add_field( 'kontrast_theme', array(
 		'social-icon'	=> array(
 			'type'			=> 'text',
 			'label'			=> esc_html__( 'Icon Name', 'kontrast' ),
-			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'kontrast' ) . ' <a href="http://fortawesome.github.io/Font-Awesome/icons/" target="_blank"><strong>' . esc_html__( 'View All', 'kontrast' ) . ' </strong></a>',
+			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'kontrast' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'kontrast' ) . ' </strong></a>',
 			'default'		=> 'fa-',
 		),
 		'social-link'	=> array(
